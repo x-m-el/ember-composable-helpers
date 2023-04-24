78b63124f1a22ce05da90567948311f4e2a774e2
Jacob Jewell <jacobjewell@eflexsystems.com>
2023-04-24 18:08:47 -0400

    simplify flatten

--- addon/helpers/flatten.js
+++ addon/helpers/flatten.js
@@ -2,13 +2,7 @@ import { helper } from '@ember/component/helper';
 import asArray from '../utils/as-array';
 
 export function flatten(array) {
-  if (!Array.isArray(array)) {
-    return array;
-  }
-
-  return asArray(array).reduce((flattened, el) => {
-    return flattened.concat(flatten(el));
-  }, []);
+  return asArray(array).flat(Infinity);
 }
 
 export default helper(function ([array]) {
